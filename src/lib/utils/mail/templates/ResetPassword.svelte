edc35147fa8d9ed83d93937b2479907d7757715c
Noam Shemesh <noams@hey.com>
2024-12-24 09:54:23 +0200

    ✨add sailkit

---
+++ src/lib/utils/mail/templates/ResetPassword.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import { Html, Head, Body, Column, Section, Text } from 'sailkit';
+
+  interface ResetPasswordEmailProps {
+    userFirstName: string;
+    url: string;
+  }
+
+  const { userFirstName, url }: ResetPasswordEmailProps = $props();
+</script>
+
+<Html>
+  <Head
+    subject="🔒 Reset Your Password"
+    preview="Reset your password securely with this link"
+    fonts={[
+      {
+        name: 'Outfit',
+        href: 'https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap'
+      }
+    ]}
+  />
+  <Body>
+    <Section>
+      <Column>
+        <Text fontFamily="Outfit" paddingBottom="0px">
+          <h1>🔒 Reset Your Password</h1>
+        </Text>
+        <Text>
+          <p>Hey, {userFirstName}!</p>
+          <p>It looks like you requested a password reset. No worries, we've got you covered!</p>
+          <p>Click the link below to reset your password:</p>
+          <p>
+            <a href={url}>Reset Your Password</a>
+          </p>
+        </Text>
+        <Column.Divider />
+        <Text>
+          <p>
+            If you didn't request this, please ignore this email or{' '}
+            <a href="https://mailto:noams+ks@hey.com">contact us</a>.
+          </p>
+          <p>Stay secure 🛟</p>
+        </Text>
+      </Column>
+    </Section>
+  </Body>
+</Html>
