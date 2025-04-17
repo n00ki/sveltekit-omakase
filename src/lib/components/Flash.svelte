374aac0466dff2577726f2747266510432d36073
Noam Shemesh <noams@hey.com>
2025-04-17 17:45:36 +0300

    💄styles: revamp flash styles

--- src/lib/components/Flash.svelte
+++ src/lib/components/Flash.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   // Utils
   import { fly } from 'svelte/transition';
+  import { cn } from '$lib/utils/utils';
 
   // Assets
   import { CheckCircled, ExclamationTriangle, CrossCircled } from 'svelte-radix';
@@ -8,34 +9,37 @@
   interface Props {
     type: 'success' | 'error' | 'warning';
     message: string;
+    class?: string;
   }
 
-  let { type, message }: Props = $props();
+  let { type, message, class: className = '' }: Props = $props();
 
-  const title = type === 'success' ? 'Success!' : type === 'error' ? 'Oops...' : 'Warning!';
-  const bgColor = type === 'error' ? 'destructive' : 'primary';
-  const textColor = type === 'error' ? 'destructive-foreground' : 'primary-foreground';
+  const baseStyles =
+    'fixed bottom-4 left-1/2 z-50 w-full max-w-sm -translate-x-1/2 rounded-lg border p-2 space-y-1 h-fit md:top-4 md:right-4 md:left-auto md:translate-x-0';
+  const typeStyles = {
+    success:
+      'border-green-100 bg-green-50 text-green-700 dark:border-green-200/10 dark:bg-green-700/10 dark:text-green-100 [&>div>svg]:text-green-700 dark:[&>div>svg]:text-green-100',
+    error:
+      'border-red-100 bg-red-50 text-red-700 dark:border-red-200/10 dark:bg-red-700/10 dark:text-red-100 [&>div>svg]:text-red-700 dark:[&>div>svg]:text-red-100',
+    warning:
+      'border-yellow-100 bg-yellow-50 text-yellow-700 dark:border-yellow-200/10 dark:bg-yellow-700/10 dark:text-yellow-100 [&>div>svg]:text-yellow-700 dark:[&>div>svg]:text-yellow-100'
+  };
 </script>
 
-<div
-  class="w-full bg-{bgColor} flex items-center gap-3 p-4 text-sm text-{textColor} [&>svg]:text-{textColor}"
-  transition:fly={{ y: 100, delay: 100 }}
->
-  <div>
-    {#if type === 'success'}
-      <CheckCircled class="h-6 w-6" />
-    {:else if type === 'error'}
-      <CrossCircled class="h-6 w-6" />
-    {:else if type === 'warning'}
-      <ExclamationTriangle class="h-6 w-6" />
-    {/if}
-  </div>
-  <div>
-    <h3 class="mb-1 leading-none font-medium tracking-tight">
-      {title}
-    </h3>
-    <p class="text-sm leading-relaxed">
+<div class={cn(baseStyles, typeStyles[type], className)} transition:fly={{ y: -20, duration: 300 }}>
+  <div class="flex items-center gap-2">
+    <div>
+      {#if type === 'success'}
+        <CheckCircled class="size-5" />
+      {:else if type === 'error'}
+        <CrossCircled class="size-5" />
+      {:else if type === 'warning'}
+        <ExclamationTriangle class="size-5" />
+      {/if}
+    </div>
+
+    <div class="text-sm">
       {message}
-    </p>
+    </div>
   </div>
 </div>
