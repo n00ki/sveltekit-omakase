3001f724414f81f25933b0622ec68f18f3952984
Noam Shemesh <noams@hey.com>
2025-10-17 18:00:07 +0300

    📦 update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
